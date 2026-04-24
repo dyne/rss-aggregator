101e29996dcd953c3d35b8f10f64a9b545cbcd3b
Jaromil <jaromil@dyne.org>
2026-04-24 23:31:48 +0200

    refactor: remove reading list ingestion

--- planet/config.py
+++ planet/config.py
@@ -18,7 +18,9 @@ Todo:
   * error handling (example: no planet section)
 """
 
-import os, sys, re, urllib.request, urllib.parse, urllib.error
+import os
+import re
+import sys
 from configparser import ConfigParser
 from urllib.parse import urljoin
 
@@ -32,8 +34,6 @@ SED_FILTERS = {
     'google_ad_map': 'stripAd/google_ad_map.sed',
     'yahoo': 'stripAd/yahoo.sed',
 }
-READING_LIST_TYPES = ('opml', 'csv', 'config')
-
 def __init__():
     """define the struture of an ini file"""
     from . import config
@@ -73,11 +73,6 @@ def __init__():
     # define a list planet-level variable
     def define_planet_int(name, default=0):
         setattr(config, name, lambda : int(get(None,name,default)))
-        planet_predefined_options.append(name)
-
-    # define a list planet-level variable
-    def define_planet_list(name, default=''):
-        setattr(config, name, lambda : expand(get(None,name,default)))
         planet_predefined_options.append(name)
 
     # define a string template-level variable
@@ -142,127 +137,10 @@ def load(config_files):
     parser = ConfigParser(interpolation=None)
     parser.read(config_files)
 
-    from . import config
     import planet
-    from planet import opml, csv_config
     log = planet.logger
     if not log:
         log = planet.getLogger(config.log_level(),config.log_format())
-
-    # Reading list support
-    reading_lists = config.reading_lists()
-    if reading_lists:
-        if not os.path.exists(config.cache_lists_directory()):
-            os.makedirs(config.cache_lists_directory())
-
-        def data2config(data, cached_config):
-            list_type = reading_list_type(list)
-            if list_type == 'opml':
-                opml.opml2config(data, cached_config)
-            elif list_type == 'csv':
-                csv_config.csv2config(data, cached_config)
-            elif list_type == 'config':
-                cached_config.read_file(data)
-            else:
-                raise ValueError("Unsupported reading list type: %s" %
-                    content_type(list))
-
-            if cached_config.sections() in [[], [list]]: 
-                raise Exception
-
-        for list in reading_lists:
-            downloadReadingList(list, parser, data2config)
-
-def downloadReadingList(list, orig_config, callback, use_cache=True, re_read=True):
-    from planet import logger
-    from . import config
-    try:
-
-        import urllib.request, urllib.error, urllib.parse, io
-        from planet.spider import filename
-
-        # list cache file name
-        cache_filename = filename(config.cache_lists_directory(), list)
-
-        # retrieve list options (e.g., etag, last-modified) from cache
-        options = {}
-
-        # add original options
-        for key in orig_config.options(list):
-            options[key] = orig_config.get(list, key)
-            
-        try:
-            if use_cache:
-                cached_config = ConfigParser(interpolation=None)
-                cached_config.read(cache_filename)
-                for option in cached_config.options(list):
-                     options[option] = cached_config.get(list,option)
-        except:
-            pass
-
-        cached_config = ConfigParser(interpolation=None)
-        cached_config.add_section(list)
-        for key, value in options.items():
-            cached_config.set(list, key, value)
-
-        # read list
-        curdir=getattr(os.path, 'curdir', '.')
-        if sys.platform.find('win') < 0:
-            base = urljoin('file:', os.path.abspath(curdir))
-        else:
-            path = os.path.abspath(os.path.curdir)
-            base = urljoin('file:///', path.replace(':','|').replace('\\','/'))
-
-        request = urllib.request.Request(urljoin(base + '/', list))
-        if "etag" in options:
-            request.add_header('If-None-Match', options['etag'])
-        if "last-modified" in options:
-            request.add_header('If-Modified-Since',
-                options['last-modified'])
-        response = urllib.request.urlopen(request)
-        if 'etag' in response.headers:
-            cached_config.set(list, 'etag', response.headers['etag'])
-        if 'last-modified' in response.headers:
-            cached_config.set(list, 'last-modified',
-                response.headers['last-modified'])
-
-        # convert to config.ini
-        response_data = response.read()
-        if isinstance(response_data, bytes):
-            response_data = response_data.decode('utf-8')
-        data = io.StringIO(response_data)
-
-        if callback: callback(data, cached_config)
-
-        # write to cache
-        if use_cache:
-            cache = open(cache_filename, 'w')
-            cached_config.write(cache)
-            cache.close()
-
-        # re-parse and proceed
-        logger.debug("Using %s readinglist", list) 
-        if re_read:
-            if use_cache:  
-                orig_config.read(cache_filename)
-            else:
-                cdata = io.StringIO()
-                cached_config.write(cdata)
-                cdata.seek(0)
-                orig_config.read_file(cdata)
-    except:
-        try:
-            if re_read:
-                if use_cache:  
-                    if not orig_config.read(cache_filename): raise Exception()
-                else:
-                    cdata = io.StringIO()
-                    cached_config.write(cdata)
-                    cdata.seek(0)
-                    orig_config.read_file(cdata)
-                logger.info("Using cached %s readinglist", list)
-        except:
-            logger.exception("Unable to read %s readinglist", list)
 
 def http_cache_directory():
     if parser.has_option('Planet', 'http_cache_directory'):
@@ -285,12 +163,6 @@ def cache_blacklist_directory():
     else:
         return os.path.join(cache_directory(), 'blacklist')
 
-def cache_lists_directory():
-    if parser.has_option('Planet', 'cache_lists_directory'):
-        return parser.get('Planet', 'cache_lists_directory')
-    else:
-        return os.path.join(cache_directory(), 'lists')
-
 def feed():
     if parser.has_option('Planet', 'feed'):
         return parser.get('Planet', 'feed')
@@ -305,27 +177,9 @@ def feedtype():
 
 def subscriptions():
     """ list the feed subscriptions """
-    return list(__builtins__['filter'](lambda feed: feed!='Planet' and 
-        feed not in reading_lists(),
+    return list(__builtins__['filter'](
+        lambda feed: feed != 'Planet',
         parser.sections()))
-
-def reading_lists():
-    """ list of lists of feed subscriptions """
-    result = []
-    for section in parser.sections():
-        if reading_list_type(section):
-            result.append(section)
-    return result
-
-def reading_list_type(section):
-    """Return the supported reading-list type for one config section."""
-    if not parser.has_option(section, 'content_type'):
-        return None
-    type = parser.get(section, 'content_type')
-    for supported in READING_LIST_TYPES:
-        if type.find(supported) >= 0:
-            return supported
-    return None
 
 def sed_filter(section=None):
     """Return the maintained sed filter path for one section."""
